477eba8a3378fb99e7d0db514f94afe7b6187ef7
rofikul-resan <resan6231@gmail.com>
2023-09-07 00:01:18 +0600

    socket server cunection bug fix

--- index.js
+++ index.js
@@ -9,14 +9,34 @@ const { Server } = require("socket.io");
 app.use(cors());
 app.use(express.json());
 const server = http.createServer(app);
-const io = new Server(server);
 
-io.on("message", () => {
-  console.log("new user");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Example: Handle 'message' event from the client
+  socket.on("message", (data) => {
+    console.log("Received message from client:", data);
+
+    // Broadcast the message to all connected clients
+    io.emit("message", data);
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
 });
 
 app.get("/", (req, res) => {
   res.send("<h1> socket server is  running </h1>");
 });
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`server os run in ${port}`);
+});
